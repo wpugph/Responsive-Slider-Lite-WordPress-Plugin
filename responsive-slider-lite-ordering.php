32ed4d3c4e9cf64e76f817207c3e387b618a8ede
carl alberto <cralberto11@gmail.com>
2016-05-21 23:19:59 +0800

    refined the sorting capability

--- responsive-slider-lite-ordering.php
+++ responsive-slider-lite-ordering.php
@@ -31,25 +31,19 @@ class Responsive_Slider_Lite_Ordering {
 	public static function load_edit_screen() {
 		$screen = get_current_screen();
 		$post_type = $screen->post_type;
-
-		// is post type sortable?
-		$sortable = ( post_type_supports( $post_type, 'page-attributes' ) || is_post_type_hierarchical( $post_type ) );		// check permission
+		$sortable = ( post_type_supports( $post_type, 'page-attributes' ) || is_post_type_hierarchical( $post_type ) );
 		if ( ! $sortable = apply_filters( 'responsive_slider_lite_ordering_is_sortable', $sortable, $post_type ) ) {
 			return;
 		}
 
-		// does user have the right to manage these post objects?
 		if ( ! self::check_edit_others_caps( $post_type ) ) {
 			return;
 		}
 
-		add_filter( 'views_' . $screen->id, array( __CLASS__, 'sort_by_order_link' )  );		// add view by menu order to views
+		add_filter( 'views_' . $screen->id, array( __CLASS__, 'sort_by_order_link' )  );
 		add_action( 'wp', array( __CLASS__, 'wp' ) );
 	}
 
-	/**
-	 * when we load up our posts query, if we're actually sorting by menu order, initialize sorting scripts
-	 */
 	public static function wp() {
 		if (get_post_type()=='responsive_slider_l') {
 			$orderby = get_query_var('orderby');
@@ -62,22 +56,18 @@ class Responsive_Slider_Lite_Ordering {
 	}
 
 	public static function ajax_responsive_slider_lite_ordering() {
-		// check and make sure we have what we need
 		if ( empty( $_POST['id'] ) || ( !isset( $_POST['previd'] ) && !isset( $_POST['nextid'] ) ) ) {
 			die(-1);
 		}
 
-		// real post?
 		if ( ! $post = get_post( $_POST['id'] ) ) {
 			die(-1);
 		}
 
-		// does user have the right to manage these post objects?
 		if ( ! self::check_edit_others_caps( $post->post_type ) ) {
 			die(-1);
 		}
 
-		// badly written plug-in hooks for save post can break things
 		if ( ! defined( 'WP_DEBUG' ) || ! WP_DEBUG ) {
 			error_reporting( 0 );
 		}
@@ -89,40 +79,37 @@ class Responsive_Slider_Lite_Ordering {
 		$start = empty( $_POST['start'] ) ? 1 : (int) $_POST['start'];
 		$excluded = empty( $_POST['excluded'] ) ? array( $post->ID ) : array_filter( (array) $_POST['excluded'], 'intval' );
 
-		$new_pos = array(); // store new positions for ajax
+		$new_pos = array();
 		$return_data = new stdClass;
 
 		do_action( 'responsive_slider_lite_ordering_pre_order_posts', $post, $start );
 
-		// attempt to get the intended parent... if either sibling has a matching parent ID, use that
 		$parent_id = $post->post_parent;
 		$next_post_parent = $nextid ? wp_get_post_parent_id( $nextid ) : false;
-		if ( $previd == $next_post_parent ) {	// if the preceding post is the parent of the next post, move it inside
+		if ( $previd == $next_post_parent ) {
 			$parent_id = $next_post_parent;
-		} elseif ( $next_post_parent !== $parent_id ) {  // otherwise, if the next post's parent isn't the same as our parent, we need to study
+		} elseif ( $next_post_parent !== $parent_id ) {
 			$prev_post_parent = $previd ? wp_get_post_parent_id( $previd ) : false;
-			if ( $prev_post_parent !== $parent_id ) {	// if the previous post is not our parent now, make it so!
+			if ( $prev_post_parent !== $parent_id ) {
 				$parent_id = ( $prev_post_parent !== false ) ? $prev_post_parent : $next_post_parent;
 			}
 		}
-		// if the next post's parent isn't our parent, it might as well be false (irrelevant to our query)
 		if ( $next_post_parent !== $parent_id ) {
 			$nextid = false;
 		}
 
-		$max_sortable_posts = (int) apply_filters( 'responsive_slider_lite_ordering_limit', 50 );	// should reliably be able to do about 50 at a time
-		if ( $max_sortable_posts < 5 ) {	// don't be ridiculous!
-			$max_sortable_posts = 50;
+		$max_sort_count = (int) apply_filters( 'responsive_slider_lite_ordering_limit', 50 );
+		if ( $max_sort_count < 5 ) {
+			$max_sort_count = 50;
 		}
 
-		// we need to handle all post stati, except trash (in case of custom stati)
 		$post_stati = get_post_stati(array(
 			'show_in_admin_all_list' => true,
 		));
 
 		$siblings_query = array(
 			'depth'						=> 1,
-			'posts_per_page'			=> $max_sortable_posts,
+			'posts_per_page'			=> $max_sort_count,
 			'post_type' 				=> $post->post_type,
 			'post_status' 				=> $post_stati,
 			'post_parent' 				=> $parent_id,
@@ -137,19 +124,15 @@ class Responsive_Slider_Lite_Ordering {
 			$siblings_query['orderby'] = 'menu_order title';
 			$siblings_query['order'] = 'ASC';
 		}
-		$siblings = new WP_Query( $siblings_query ); // fetch all the siblings (relative ordering)
-
-		// don't waste overhead of revisions on a menu order change (especially since they can't *all* be rolled back at once)
+		$siblings = new WP_Query( $siblings_query );
 		remove_action( 'pre_post_update', 'wp_save_post_revision' );
 
 		foreach( $siblings->posts as $sibling ) :
 
-			// don't handle the actual post
 			if ( $sibling->ID === $post->ID ) {
 				continue;
 			}
 
-			// if this is the post that comes after our repositioned post, set our repositioned post position and increment menu order
 			if ( $nextid === $sibling->ID ) {
 				wp_update_post(array(
 					'ID'			=> $post->ID,
@@ -165,13 +148,11 @@ class Responsive_Slider_Lite_Ordering {
 				$start++;
 			}
 
-			// if repositioned post has been set, and new items are already in the right order, we can stop
 			if ( isset( $new_pos[$post->ID] ) && $sibling->menu_order >= $start ) {
 				$return_data->next = false;
 				break;
 			}
 
-			// set the menu order of the current sibling and increment the menu order
 			if ( $sibling->menu_order != $start ) {
 				wp_update_post(array(
 					'ID' 			=> $sibling->ID,
@@ -197,7 +178,6 @@ class Responsive_Slider_Lite_Ordering {
 
 		endforeach;
 
-		// max per request
 		if ( !isset( $return_data->next ) && $siblings->max_num_pages > 1 ) {
 			$return_data->next = array(
 				'id' 		=> $post->ID,
@@ -213,7 +193,6 @@ class Responsive_Slider_Lite_Ordering {
 		do_action( 'responsive_slider_lite_ordering_ordered_posts', $post, $new_pos );
 
 		if ( ! $return_data->next ) {
-			// if the moved post has children, we need to refresh the page (unless we're continuing)
 			$children = get_posts(array(
 				'numberposts'				=> 1,
 				'post_type' 				=> $post->post_type,
@@ -234,12 +213,6 @@ class Responsive_Slider_Lite_Ordering {
 		die( json_encode( $return_data ) );
 	}
 
-	/**
-	 * Append a sort by order link to the post actions
-	 *
-	 * @param string $views
-	 * @return string
-	 */
 	public static function sort_by_order_link( $views ) {
 		$class = ( get_query_var('orderby') == 'menu_order title' ) ? 'current' : '';
 		$query_string = esc_url( remove_query_arg( array( 'orderby', 'order' ) ) );
@@ -252,12 +225,6 @@ class Responsive_Slider_Lite_Ordering {
 		return $views;
 	}
 
-	/**
-	 * Checks to see if the current user has the capability to "edit others" for a post type
-	 *
-	 * @param string $post_type Post type name
-	 * @return bool True or false
-	 */
 	private static function check_edit_others_caps( $post_type ) {
 		$post_type_object = get_post_type_object( $post_type );
 		$edit_others_cap = empty( $post_type_object ) ? 'edit_others_' . $post_type . 's' : $post_type_object->cap->edit_others_posts;
